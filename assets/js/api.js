93d9b7fcfb04c008ac63fb76113519624e11ad8a
Christo <fire95.wow@gmail.com>
2020-10-13 08:24:40 +0200

    Amélioration selection

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,5 @@
 const categBlock = document.getElementById('categBlock');
+let preventTarget = null;
 
 fetch('https://opentdb.com/api_category.php').then((response) => {
     response.json().then((jsonObject) => {
@@ -15,6 +16,10 @@ fetch('https://opentdb.com/api_category.php').then((response) => {
 categBlock.addEventListener('click', (event) => {
     let target = event.target;
 
+    if (preventTarget !== null) {
+        preventTarget.removeAttribute('style');
+    }
+    preventTarget = target;
     if (target.tagName === 'P') { target = target.parentElement; }
 
     target.style.border = "solid red 3px";

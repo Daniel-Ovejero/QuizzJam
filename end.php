519fbfd1a8a5b3f3c576c48b8e461529d92f96b0
Christo <fire95.wow@gmail.com>
2020-10-13 15:18:56 +0200

    Page de fin

--- end.php
+++ end.php
@@ -7,14 +7,30 @@ session_start();
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/animation.css">
     <title>QuizzJam</title>
 </head>
 <body>
 
 
+    <div class="container" style="position: relative; z-index: 100">
+        <div style="margin-top: 20%;">
+
+            <h1 class="text-center">Félicitation</h1>
+            <h4 class="text-center">Votre score est de </h4>
+            <br>
+            <div class="row h-100 justify-content-center align-items-center center-block">
+                <img class="img-fluid rounded-circle" style="width: 35%;" src="./assets/images/trophe.png" alt="Trophe">
+            </div>
+        </div>
+
+    </div>
+    <canvas style="position: absolute; z-index: -1; top: -10px;" id="canvas"></canvas>
+
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<script src="./assets/js/api.js"></script>
+<script src="./assets/js/animation.js"></script>
+
 </body>
 </html>
